25b985d5e9138207ea35437ae2466b968accab69
Eka Priyanthara <ekapriyanthara072@gmail.com>
2023-10-25 18:40:59 +0800

    first commit

---
+++ src/database.js
@@ -0,0 +1,75 @@
+import img1 from "./assets/item-1.jpeg";
+import img2 from "./assets/item-2.jpeg";
+import img3 from "./assets/item-3.jpeg";
+import img4 from "./assets/item-4.jpeg";
+import img5 from "./assets/item-5.jpeg";
+import img6 from "./assets/item-6.jpeg";
+import img7 from "./assets/item-7.jpeg";
+import img8 from "./assets/item-8.jpeg";
+
+export const menu = [
+  {
+    image: img1,
+    name: "Buttermilk Pancakes",
+    price: 15.99,
+    category: "breakfast",
+    description:
+      "Fluffy, tangy buttermilk pancakes: a golden, indulgent breakfast delight that's simply irresistible and delicious.",
+  },
+  {
+    image: img2,
+    name: "Diner Double",
+    price: 13.99,
+    category: "lunch",
+    description:
+      "This is a towering, cheesy burger with juicy patties, fresh toppings, and irresistible flavors.",
+  },
+  {
+    image: img3,
+    name: "Godzilla Milkshake",
+    price: 6.99,
+    category: "shakes",
+    description:
+      "A monstrous, towering milkshake with flavors that will shake your taste buds to their core.",
+  },
+  {
+    image: img4,
+    name: "Country Delight",
+    price: 20.99,
+    category: "breakfast",
+    description:
+      "Golden-brown toasted bread topped with a luscious egg and drizzled with tangy tomato sauce. Simply delicious!",
+  },
+  {
+    image: img5,
+    name: "Egg Attack",
+    price: 22.99,
+    category: "lunch",
+    description:
+      "A mouthwatering combination of a perfectly cooked egg and a juicy, flavorful burger patty.",
+  },
+  {
+    image: img6,
+    name: "Oreo Dream",
+    price: 18.99,
+    category: "shakes",
+    description:
+      "Creamy milkshakes with crushed Oreo cookies, delivering a dreamy blend of indulgence and nostalgia.",
+  },
+  {
+    image: img7,
+    name: "Bacon Overflow",
+    price: 8.99,
+    category: "breakfast",
+    description:
+      "A mouthwatering burger piled high with crispy bacon, oozing cheese, and overflowing with savory goodness.",
+  },
+  {
+    image: img8,
+    name: "American Classic",
+    price: 12.99,
+    category: "lunch",
+    description:
+      "A classic, no-frills burger with a juicy beef patty, fresh toppings, and a soft bun.",
+  },
+];
